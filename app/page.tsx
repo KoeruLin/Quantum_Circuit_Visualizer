229da547e4f258ddd5111b0727177b9cf48ac0c2
Ryan Lin <rlin991215@gmail.com>
2026-02-25 18:14:58 -0500

    Initial commit from Create Next App

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,3 @@
-import query, {CircuitStructure, GateStructure} from "./quantum_caller"
-
-const JSON_OBJECT: GateStructure = {
-    qubits: [0],
-    gate: "H",
-    theta: 0.0,
-    phi: 0.0,
-    lamda: 0.0
-}
-const JSON_OBJECT2: GateStructure = {
-    qubits: [0],
-    gate: "Z",
-    theta: 0.0,
-    phi: 0.0,
-    lamda: 0.0
-}
-
-const JSON_LIST: CircuitStructure = {
-    circuit: [JSON_OBJECT, JSON_OBJECT2]
-}
-
 export default function Home() {
     return (
         <html>
